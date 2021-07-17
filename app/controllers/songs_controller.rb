019cdf30cd6f6159bc72d67239c203b40d9689f6
bcharlotin1 <biancacharlotin1@gmail.com>
2021-07-17 11:19:38 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,7 +47,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:genre_name, :artist_name, :title)
+    params.require(:song).permit(:genre_id, :artist_name, :title, :note_info => [])
   end
 end
 
